b49da7bb4f09372d00aa58a7fc6946eb3c07454c
Dave <146802607+Saiganity1@users.noreply.github.com>
2026-02-10 13:56:20 +0800

    Final Change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header surname="Sicat" />
+      <Header surname="SICAT" />
       <Box subjectCode="C-PEITEL3" />
     </div>
   );
